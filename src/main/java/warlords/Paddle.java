c0c3274bdc7d1314df31df7f5e4bbcc96ceeab97
Hamish O'Neill <hone075@aucklanduni.ac.nz>
2017-03-25 18:09:25 +1300

    Implement functionality related to rotation and hit boxes in the GameObject base class.

--- src/main/java/warlords/Paddle.java
+++ src/main/java/warlords/Paddle.java
@@ -8,7 +8,6 @@ import static java.lang.Math.*;
 
 public class Paddle extends GameObject implements IPaddle {
 	private Game game;
-	private Double theta;
 	private Double theta_init;
 	private int x_init;
 	private int y_init;
@@ -16,19 +15,14 @@ public class Paddle extends GameObject implements IPaddle {
 	private double ANGLE_DIFF = PI/150;
 
 	public Paddle(int x, int y, Double theta, Game game) {
-		super(x, y, "/red.png");
+		super(x, y, "/red.png", theta + PI/4);
 		this.width = 100;
 		this.height = 20;
 		x_init = x;
 		y_init = y;
 		theta_init = theta;
 		this.game = game;
-		this.theta = theta + PI/4;
 		setPosition();
-	}
-
-	public Double getAngle() {
-		return theta;
 	}
 
 	@Override
@@ -52,35 +46,21 @@ public class Paddle extends GameObject implements IPaddle {
 	}
 
 	private void moveCW() {
-		if (theta < theta_init+ PI/2) {
-			theta += ANGLE_DIFF;
+		if (rotationAngle < theta_init+ PI/2) {
+			rotationAngle += ANGLE_DIFF;
 			setPosition();
 		}
 	}
 
 	private void moveCCW() {
-		if (theta > theta_init) {
-			theta -= ANGLE_DIFF;
+		if (rotationAngle > theta_init) {
+			rotationAngle -= ANGLE_DIFF;
 			setPosition();
 		}
 	}
 
 	private void setPosition() {
-		x = (int)((5.0/18.0)*game.getWidth()*cos(theta))+x_init;
-		y = (int)((5.0/12.0)*game.getHeight()*sin(theta))+y_init;
-	}
-
-	@Override
-	public Double getRotation() {
-		return theta - PI/2;
-	}
-
-	@Override
-	public Shape getRectangle() {
-
-		Shape shape = new Rectangle2D.Double(x - width/2, y - height/2, width, height);
-		AffineTransform at = AffineTransform.getRotateInstance(theta - PI/2, x, y);
-		shape = at.createTransformedShape(shape);
-		return shape;
+		x = (5.0/18.0)*game.getWidth()*cos(rotationAngle) + x_init;
+		y = (5.0/12.0)*game.getHeight()*sin(rotationAngle) + y_init;
 	}
 }
--- src/main/java/warlords/Wall.java
+++ src/main/java/warlords/Wall.java
@@ -8,12 +8,10 @@ import static java.lang.Math.PI;
 
 public class Wall extends GameObject implements IWall {
 
-	private double theta;
 	private int health = 1;
 
 	public Wall(int x, int y, double theta) {
-		super(x, y, null);
-		this.theta = theta;
+		super(x, y, null, theta);
 	}
 
 	@Override
@@ -24,20 +22,6 @@ public class Wall extends GameObject implements IWall {
 	@Override
 	public void tick() {
 
-	}
-
-	@Override
-	public Double getRotation() {
-		return theta - PI/2;
-	}
-
-	@Override
-	public Shape getRectangle() {
-
-		Shape shape = new Rectangle2D.Double(x - width/2, y - height/2, width, height);
-		AffineTransform at = AffineTransform.getRotateInstance(theta - PI/2, x, y);
-		shape = at.createTransformedShape(shape);
-		return shape;
 	}
 
 	public void causeDamage(int damage) {
