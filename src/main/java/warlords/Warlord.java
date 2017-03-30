523bf245e0a0b3eaae5e7a6bf6b60b100933e3d2
Hamish O'Neill <hone075@aucklanduni.ac.nz>
2017-03-30 16:48:34 +1300

    JavaDoc comments for 3 more classes.

--- src/main/java/warlords/Warlord.java
+++ src/main/java/warlords/Warlord.java
@@ -2,11 +2,21 @@ package warlords;
 
 import warlordstest.IWarlord;
 
+/**
+ * Represents a single instance of a warlord in the game.
+ * Also stores whether that player has won the game.
+ */
 public class Warlord extends GameObject implements IWarlord {
 
 	private boolean hasWon = false;
 	private int health = 1;
 
+	/**
+	 * Create a new Warlord at the given position with the given sprite
+	 * @param x the x position on the screen
+	 * @param y the y position on the screen
+	 * @param imgPath the path to the sprite image file
+	 */
 	public Warlord(int x, int y, String imgPath) {
 		super(x, y, imgPath, Math.PI/2);
 		this.width = 100;
