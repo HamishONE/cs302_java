2a168118fab31ba9bce8ca86c19c14c3f11beb53
Roman Amor <romanwilton@gmail.com>
2017-04-02 00:07:56 +1300

    Resolved merge conflict with local stash of sound player files, to be immediately changed for a proper implementation

--- src/main/java/warlords/Wall.java
+++ src/main/java/warlords/Wall.java
@@ -2,6 +2,12 @@ package warlords;
 
 import warlordstest.IWall;
 
+//TODO remove once sound is properly implemented
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+
+
 /**
  * Represents a single instance of a wall/brick in the game.
  */
@@ -9,6 +15,11 @@ public class Wall extends GameObject implements IWall {
 
 	private int health = 1;
 	private int owner;
+
+
+	//TODO remove once sound is properly implemented
+	private MediaPlayer mediaPlayer;
+
 
 	/**
 	 * Create a new wall at a given position with a given owner
@@ -22,6 +33,12 @@ public class Wall extends GameObject implements IWall {
 		width = 44.6;
 		height = 20.9;
 		this.owner = owner;
+
+
+		//TODO remove once sound is properly implemented
+		String musicFile = "build/resources/main/Sad-cat.mp3";
+		Media sound = new Media(new File(musicFile).toURI().toString());
+		mediaPlayer = new MediaPlayer(sound);
 	}
 
 	@Override
@@ -35,6 +52,9 @@ public class Wall extends GameObject implements IWall {
 	 */
 	public void causeDamage(int damage) {
 		health -= damage;
+
+		//TODO remove once sound is properly implemented
+		mediaPlayer.play();
 	}
 
 	/**
