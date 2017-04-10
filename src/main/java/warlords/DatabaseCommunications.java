7a70b2560163a34ec63870cd17b4caacdf4cdc1b
Roman Amor <romanwilton@gmail.com>
2017-04-11 10:39:42 +1200

    Cleaned up and fixed web based high scores Now doesn't make an http request every frame.

--- src/main/java/warlords/DatabaseCommunications.java
+++ src/main/java/warlords/DatabaseCommunications.java
@@ -1,6 +1,8 @@
 package warlords;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -12,6 +14,11 @@ public class DatabaseCommunications {
 
 	private String putUrl = "http://romana.co.nz/CS302/insert.php";
 	private String getUrl = "http://romana.co.nz/CS302/getScores.php";
+	private ArrayList<Score> scores = new ArrayList<>();
+
+	public DatabaseCommunications() {
+		loadValues();
+	}
 
 	public boolean putValues(String name, int score) {
 		try {
@@ -19,45 +26,48 @@ public class DatabaseCommunications {
 			connection.setRequestProperty("Accept-Charset", java.nio.charset.StandardCharsets.UTF_8.name());
 			InputStream response = connection.getInputStream();
 
-			try (Scanner scanner = new Scanner(response)) {
-				String responseBody = scanner.next();
-				if(responseBody == "New record created successfully") {
-					return true;
-				}
+			Scanner scanner = new Scanner(response);
+			String responseBody = scanner.next();
+			if(responseBody.equals("Success")) {
+				loadValues();
+				return true;
 			}
-		} catch (Exception ex) {
-			return false;
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
 		return false;
 	}
 
 	
-	public ArrayList<Score> getValues() {
+	public void loadValues() {
 		try {
 			URLConnection connection = new URL(getUrl).openConnection();
 			connection.setRequestProperty("Accept-Charset", java.nio.charset.StandardCharsets.UTF_8.name());
 			InputStream response = connection.getInputStream();
 
-			try (Scanner scanner = new Scanner(response)) {
-				if (scanner.hasNext() == false) {
-					return new ArrayList<Score>();
-				}
+			Scanner scanner = new Scanner(response);
+			if (scanner.hasNext()) {
 				String responseBody = scanner.next();
 				List<String> tempList = Arrays.asList(responseBody.split("\\|"));
 				ArrayList<String> scoreString = new ArrayList<>(tempList);
+
 				if (scoreString.size() > 1) {
 					ArrayList<Score> scores = new ArrayList<>();
 					for (int i = 0; i < scoreString.size(); i += 2) {
 						scores.add(new Score(scoreString.get(i), Integer.parseInt(scoreString.get(i + 1))));
 					}
-					return scores;
+					this.scores = scores;
 				}
 			}
-		} catch (Exception ex) {
-			//DO NOTHING useful
-			System.out.println(ex);
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-		return new ArrayList<Score>();
 	}
+
+	public ArrayList<Score> getValues() {
+		return scores;
+	}
+
+
 
 }
