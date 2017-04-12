e2f151675edc2b695a9899c382c159f67ff42737
Roman Amor <romanwilton@gmail.com>
2017-04-13 11:51:36 +1200

    Added file that can be called on the highscores database to reset and refill the highscores with default values

---
+++ WebSource/RefillDB.sql
@@ -0,0 +1,8 @@
+DELETE FROM CS302_Highscores WHERE 1;
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('Roman', 12);
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('Hamish', 12);
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('Izzy', 13);
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('Ben', 2);
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('Josh', 5);
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('Andrew', 11);
+INSERT INTO `CS302_Highscores`(`Name`, `Score`) VALUES ('I am losing', 1);
